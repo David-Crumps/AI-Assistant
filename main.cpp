1b43abde86461d12df947de36334197b5f2614a8
David-Crumps <david.crumplin@gmail.com>
2025-09-11 11:04:34 +0200

    Project

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#include <cstdlib>
+#include <windows.h>
+
+#include <stdio.h>
+#include <string>
+#include <lmcons.h>
+
+#include <ctime>
+#include <chrono>
+
+
+void outputToVoice(std::string phrase) {
+    std::string command = "espeak \"" + phrase + "\"";
+    system(command.c_str());
+}
+
+void greeting() {
+    std::string output;
+
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+    std::tm* local_tm = std::localtime(&now_c);
+
+    output = (local_tm->tm_hour < 12) ? "Good Morning" :
+        (local_tm->tm_hour >= 12 && local_tm->tm_hour < 17) ? "Good Afternoon" : "Good Evening";
+
+    TCHAR username[UNLEN+1];
+    DWORD username_len = UNLEN+1;
+
+    if (GetUserName(username, &username_len)) {
+            outputToVoice(output+" "+username);
+    }
+    else {
+        std::cerr << "Invalid User" << std::endl;
+        exit(0);
+    }
+}
+
+int main()
+{
+    system("cls");
+    std::cout << "*******************************" << std::endl;
+    std::cout << "PERSONAL ASSISSTANT INTIALISING" << std::endl;
+    std::cout << "*******************************" << std::endl;
+    greeting();
+    int x = 5;
+
+    return 0;
+}
